acca7399091577beef70563fbe6ba9e8c78ce7e7
Amin Mir <aminmir326@gmail.com>
2019-07-21 11:28:10 +0430

    Feature/admin api

---
+++ admin/nats.go
@@ -0,0 +1,18 @@
+package admin
+
+import (
+	"time"
+)
+
+const (
+	UpsertUserSubj        = "upsert.user"
+	UpsertUserGroup       = "upsert.user"
+	UpsertUserDurName     = "upsert.user"
+	UpsertUserAckWait     = 5 * time.Second
+	UpsertUserMaxInflight = 50
+
+	NewWelcMessageSubj  = "new.welc.msg"
+
+	GetWelcMessageSubj  = "get.welc.msg"
+	GetWelcMessageGroup = "get.welc.msg"
+)

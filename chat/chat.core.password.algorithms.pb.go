56fd2b3fb4e7dc1ac58918c2b0e5664936cdd072
Ehsan N. Moosa <ehsan.code@nested.me>
2020-01-07 00:26:21 +0330

    2FA messages added

--- chat/chat.core.password.algorithms.pb.go
+++ chat/chat.core.password.algorithms.pb.go
@@ -23,25 +23,25 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
-type PasswordAlgorithmKdfSha512Iter100000 struct {
+type PasswordAlgorithmVer6A struct {
 	Salt1 []byte `protobuf:"bytes,1,req,name=Salt1" json:"Salt1"`
 	Salt2 []byte `protobuf:"bytes,2,req,name=Salt2" json:"Salt2"`
 	G     int32  `protobuf:"varint,3,req,name=G" json:"G"`
 	P     []byte `protobuf:"bytes,4,req,name=P" json:"P"`
 }
 
-func (m *PasswordAlgorithmKdfSha512Iter100000) Reset()         { *m = PasswordAlgorithmKdfSha512Iter100000{} }
-func (m *PasswordAlgorithmKdfSha512Iter100000) String() string { return proto.CompactTextString(m) }
-func (*PasswordAlgorithmKdfSha512Iter100000) ProtoMessage()    {}
-func (*PasswordAlgorithmKdfSha512Iter100000) Descriptor() ([]byte, []int) {
+func (m *PasswordAlgorithmVer6A) Reset()         { *m = PasswordAlgorithmVer6A{} }
+func (m *PasswordAlgorithmVer6A) String() string { return proto.CompactTextString(m) }
+func (*PasswordAlgorithmVer6A) ProtoMessage()    {}
+func (*PasswordAlgorithmVer6A) Descriptor() ([]byte, []int) {
 	return fileDescriptor_aea8cee14a2df555, []int{0}
 }
-func (m *PasswordAlgorithmKdfSha512Iter100000) XXX_Unmarshal(b []byte) error {
+func (m *PasswordAlgorithmVer6A) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
 }
-func (m *PasswordAlgorithmKdfSha512Iter100000) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+func (m *PasswordAlgorithmVer6A) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	if deterministic {
-		return xxx_messageInfo_PasswordAlgorithmKdfSha512Iter100000.Marshal(b, m, deterministic)
+		return xxx_messageInfo_PasswordAlgorithmVer6A.Marshal(b, m, deterministic)
 	} else {
 		b = b[:cap(b)]
 		n, err := m.MarshalToSizedBuffer(b)
@@ -51,40 +51,40 @@ func (m *PasswordAlgorithmKdfSha512Iter100000) XXX_Marshal(b []byte, determinist
 		return b[:n], nil
 	}
 }
-func (m *PasswordAlgorithmKdfSha512Iter100000) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_PasswordAlgorithmKdfSha512Iter100000.Merge(m, src)
+func (m *PasswordAlgorithmVer6A) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_PasswordAlgorithmVer6A.Merge(m, src)
 }
-func (m *PasswordAlgorithmKdfSha512Iter100000) XXX_Size() int {
+func (m *PasswordAlgorithmVer6A) XXX_Size() int {
 	return m.Size()
 }
-func (m *PasswordAlgorithmKdfSha512Iter100000) XXX_DiscardUnknown() {
-	xxx_messageInfo_PasswordAlgorithmKdfSha512Iter100000.DiscardUnknown(m)
+func (m *PasswordAlgorithmVer6A) XXX_DiscardUnknown() {
+	xxx_messageInfo_PasswordAlgorithmVer6A.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_PasswordAlgorithmKdfSha512Iter100000 proto.InternalMessageInfo
+var xxx_messageInfo_PasswordAlgorithmVer6A proto.InternalMessageInfo
 
-func (m *PasswordAlgorithmKdfSha512Iter100000) GetSalt1() []byte {
+func (m *PasswordAlgorithmVer6A) GetSalt1() []byte {
 	if m != nil {
 		return m.Salt1
 	}
 	return nil
 }
 
-func (m *PasswordAlgorithmKdfSha512Iter100000) GetSalt2() []byte {
+func (m *PasswordAlgorithmVer6A) GetSalt2() []byte {
 	if m != nil {
 		return m.Salt2
 	}
 	return nil
 }
 
-func (m *PasswordAlgorithmKdfSha512Iter100000) GetG() int32 {
+func (m *PasswordAlgorithmVer6A) GetG() int32 {
 	if m != nil {
 		return m.G
 	}
 	return 0
 }
 
-func (m *PasswordAlgorithmKdfSha512Iter100000) GetP() []byte {
+func (m *PasswordAlgorithmVer6A) GetP() []byte {
 	if m != nil {
 		return m.P
 	}
@@ -92,7 +92,7 @@ func (m *PasswordAlgorithmKdfSha512Iter100000) GetP() []byte {
 }
 
 func init() {
-	proto.RegisterType((*PasswordAlgorithmKdfSha512Iter100000)(nil), "msg.PasswordAlgorithmKdfSha512Iter100000")
+	proto.RegisterType((*PasswordAlgorithmVer6A)(nil), "msg.PasswordAlgorithmVer6A")
 }
 
 func init() {
@@ -100,22 +100,21 @@ func init() {
 }
 
 var fileDescriptor_aea8cee14a2df555 = []byte{
-	// 177 bytes of a gzipped FileDescriptorProto
+	// 164 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x52, 0x4e, 0xce, 0x48, 0x2c,
 	0xd1, 0x4b, 0xce, 0x2f, 0x4a, 0xd5, 0x2b, 0x48, 0x2c, 0x2e, 0x2e, 0xcf, 0x2f, 0x4a, 0xd1, 0x4b,
 	0xcc, 0x49, 0xcf, 0x2f, 0xca, 0x2c, 0xc9, 0xc8, 0x2d, 0xd6, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17,
-	0x62, 0xce, 0x2d, 0x4e, 0x57, 0x6a, 0x63, 0xe4, 0x52, 0x09, 0x80, 0x2a, 0x71, 0x84, 0xa9, 0xf0,
-	0x4e, 0x49, 0x0b, 0xce, 0x48, 0x34, 0x35, 0x34, 0xf2, 0x2c, 0x49, 0x2d, 0x32, 0x34, 0x00, 0x01,
-	0x21, 0x29, 0x2e, 0xd6, 0xe0, 0xc4, 0x9c, 0x12, 0x43, 0x09, 0x46, 0x05, 0x26, 0x0d, 0x1e, 0x27,
-	0x96, 0x13, 0xf7, 0xe4, 0x19, 0x82, 0x20, 0x42, 0x30, 0x39, 0x23, 0x09, 0x26, 0x74, 0x39, 0x23,
-	0x21, 0x21, 0x2e, 0x46, 0x77, 0x09, 0x66, 0x05, 0x26, 0x0d, 0x56, 0xa8, 0x38, 0xa3, 0x3b, 0x48,
-	0x2c, 0x40, 0x82, 0x05, 0x49, 0x2d, 0x63, 0x80, 0x93, 0xc4, 0x89, 0x47, 0x72, 0x8c, 0x17, 0x1e,
-	0xc9, 0x31, 0x3e, 0x78, 0x24, 0xc7, 0x38, 0xe1, 0xb1, 0x1c, 0xc3, 0x85, 0xc7, 0x72, 0x0c, 0x37,
-	0x1e, 0xcb, 0x31, 0x00, 0x02, 0x00, 0x00, 0xff, 0xff, 0x68, 0x8e, 0x3b, 0x43, 0xcd, 0x00, 0x00,
-	0x00,
+	0x62, 0xce, 0x2d, 0x4e, 0x57, 0xaa, 0xe0, 0x12, 0x0b, 0x80, 0xaa, 0x70, 0x84, 0x29, 0x08, 0x4b,
+	0x2d, 0x32, 0x73, 0x14, 0x92, 0xe2, 0x62, 0x0d, 0x4e, 0xcc, 0x29, 0x31, 0x94, 0x60, 0x54, 0x60,
+	0xd2, 0xe0, 0x71, 0x62, 0x39, 0x71, 0x4f, 0x9e, 0x21, 0x08, 0x22, 0x04, 0x93, 0x33, 0x92, 0x60,
+	0x42, 0x97, 0x33, 0x12, 0x12, 0xe2, 0x62, 0x74, 0x97, 0x60, 0x56, 0x60, 0xd2, 0x60, 0x85, 0x8a,
+	0x33, 0xba, 0x83, 0xc4, 0x02, 0x24, 0x58, 0x90, 0xd4, 0x32, 0x06, 0x38, 0x49, 0x9c, 0x78, 0x24,
+	0xc7, 0x78, 0xe1, 0x91, 0x1c, 0xe3, 0x83, 0x47, 0x72, 0x8c, 0x13, 0x1e, 0xcb, 0x31, 0x5c, 0x78,
+	0x2c, 0xc7, 0x70, 0xe3, 0xb1, 0x1c, 0x03, 0x20, 0x00, 0x00, 0xff, 0xff, 0x23, 0xe7, 0x70, 0x44,
+	0xbe, 0x00, 0x00, 0x00,
 }
 
-func (m *PasswordAlgorithmKdfSha512Iter100000) Marshal() (dAtA []byte, err error) {
+func (m *PasswordAlgorithmVer6A) Marshal() (dAtA []byte, err error) {
 	size := m.Size()
 	dAtA = make([]byte, size)
 	n, err := m.MarshalToSizedBuffer(dAtA[:size])
@@ -125,12 +124,12 @@ func (m *PasswordAlgorithmKdfSha512Iter100000) Marshal() (dAtA []byte, err error
 	return dAtA[:n], nil
 }
 
-func (m *PasswordAlgorithmKdfSha512Iter100000) MarshalTo(dAtA []byte) (int, error) {
+func (m *PasswordAlgorithmVer6A) MarshalTo(dAtA []byte) (int, error) {
 	size := m.Size()
 	return m.MarshalToSizedBuffer(dAtA[:size])
 }
 
-func (m *PasswordAlgorithmKdfSha512Iter100000) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+func (m *PasswordAlgorithmVer6A) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	i := len(dAtA)
 	_ = i
 	var l int
@@ -173,7 +172,7 @@ func encodeVarintChatCorePasswordAlgorithms(dAtA []byte, offset int, v uint64) i
 	dAtA[offset] = uint8(v)
 	return base
 }
-func (m *PasswordAlgorithmKdfSha512Iter100000) Size() (n int) {
+func (m *PasswordAlgorithmVer6A) Size() (n int) {
 	if m == nil {
 		return 0
 	}
@@ -201,7 +200,7 @@ func sovChatCorePasswordAlgorithms(x uint64) (n int) {
 func sozChatCorePasswordAlgorithms(x uint64) (n int) {
 	return sovChatCorePasswordAlgorithms(uint64((x << 1) ^ uint64((int64(x) >> 63))))
 }
-func (m *PasswordAlgorithmKdfSha512Iter100000) Unmarshal(dAtA []byte) error {
+func (m *PasswordAlgorithmVer6A) Unmarshal(dAtA []byte) error {
 	var hasFields [1]uint64
 	l := len(dAtA)
 	iNdEx := 0
@@ -225,10 +224,10 @@ func (m *PasswordAlgorithmKdfSha512Iter100000) Unmarshal(dAtA []byte) error {
 		fieldNum := int32(wire >> 3)
 		wireType := int(wire & 0x7)
 		if wireType == 4 {
-			return fmt.Errorf("proto: PasswordAlgorithmKdfSha512Iter100000: wiretype end group for non-group")
+			return fmt.Errorf("proto: PasswordAlgorithmVer6A: wiretype end group for non-group")
 		}
 		if fieldNum <= 0 {
-			return fmt.Errorf("proto: PasswordAlgorithmKdfSha512Iter100000: illegal tag %d (wire type %d)", fieldNum, wire)
+			return fmt.Errorf("proto: PasswordAlgorithmVer6A: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
 		case 1:

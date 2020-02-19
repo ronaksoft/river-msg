21a9570845852386608809e7fd010061b5e3e6dc
Reza Pilehvar <graphreza@gmail.com>
2020-02-19 11:53:34 +0330

    build client get media history

--- chat/chat.api.updates.pb.go
+++ chat/chat.api.updates.pb.go
@@ -10710,7 +10710,6 @@ func (m *UpdateMessagePoll) Unmarshal(dAtA []byte) error {
 func skipChatApiUpdates(dAtA []byte) (n int, err error) {
 	l := len(dAtA)
 	iNdEx := 0
-	depth := 0
 	for iNdEx < l {
 		var wire uint64
 		for shift := uint(0); ; shift += 7 {
@@ -10742,8 +10741,10 @@ func skipChatApiUpdates(dAtA []byte) (n int, err error) {
 					break
 				}
 			}
+			return iNdEx, nil
 		case 1:
 			iNdEx += 8
+			return iNdEx, nil
 		case 2:
 			var length int
 			for shift := uint(0); ; shift += 7 {
@@ -10764,30 +10765,55 @@ func skipChatApiUpdates(dAtA []byte) (n int, err error) {
 				return 0, ErrInvalidLengthChatApiUpdates
 			}
 			iNdEx += length
-		case 3:
-			depth++
-		case 4:
-			if depth == 0 {
-				return 0, ErrUnexpectedEndOfGroupChatApiUpdates
+			if iNdEx < 0 {
+				return 0, ErrInvalidLengthChatApiUpdates
 			}
-			depth--
+			return iNdEx, nil
+		case 3:
+			for {
+				var innerWire uint64
+				var start int = iNdEx
+				for shift := uint(0); ; shift += 7 {
+					if shift >= 64 {
+						return 0, ErrIntOverflowChatApiUpdates
+					}
+					if iNdEx >= l {
+						return 0, io.ErrUnexpectedEOF
+					}
+					b := dAtA[iNdEx]
+					iNdEx++
+					innerWire |= (uint64(b) & 0x7F) << shift
+					if b < 0x80 {
+						break
+					}
+				}
+				innerWireType := int(innerWire & 0x7)
+				if innerWireType == 4 {
+					break
+				}
+				next, err := skipChatApiUpdates(dAtA[start:])
+				if err != nil {
+					return 0, err
+				}
+				iNdEx = start + next
+				if iNdEx < 0 {
+					return 0, ErrInvalidLengthChatApiUpdates
+				}
+			}
+			return iNdEx, nil
+		case 4:
+			return iNdEx, nil
 		case 5:
 			iNdEx += 4
+			return iNdEx, nil
 		default:
 			return 0, fmt.Errorf("proto: illegal wireType %d", wireType)
 		}
-		if iNdEx < 0 {
-			return 0, ErrInvalidLengthChatApiUpdates
-		}
-		if depth == 0 {
-			return iNdEx, nil
-		}
 	}
-	return 0, io.ErrUnexpectedEOF
+	panic("unreachable")
 }
 
 var (
-	ErrInvalidLengthChatApiUpdates        = fmt.Errorf("proto: negative length found during unmarshaling")
-	ErrIntOverflowChatApiUpdates          = fmt.Errorf("proto: integer overflow")
-	ErrUnexpectedEndOfGroupChatApiUpdates = fmt.Errorf("proto: unexpected end of group")
+	ErrInvalidLengthChatApiUpdates = fmt.Errorf("proto: negative length found during unmarshaling")
+	ErrIntOverflowChatApiUpdates   = fmt.Errorf("proto: integer overflow")
 )

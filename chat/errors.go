4d9ff29e50946c7bd2fd2995dc46f9e55d084791
Ehsan N. Moosa <ehsan.code@nested.me>
2020-05-10 12:09:36 +0430

    error item added DOCUMENT_TYPE

--- chat/errors.go
+++ chat/errors.go
@@ -69,6 +69,7 @@ const (
 	ErrItemDeviceToken      = "DEVICE_TOKEN"
 	ErrItemDeviceModel      = "DEVICE_MODEL"
 	ErrItemDocument         = "DOCUMENT"
+	ErrItemDocumentType     = "DOCUMENT_TYPE"
 	ErrItemToken            = "TOKEN"
 	ErrItemMedia            = "MEDIA"
 	ErrItemPinnedDialogs    = "PINNED_DIALOGS"

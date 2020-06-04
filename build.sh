3e390cd800ad2c438a98dfdb19767c530165cccb
Ehsan N. Moosa <ehsan.code@nested.me>
2020-06-04 16:53:27 +0430

    BIG refactor, ready for Teams, Nested, ...

--- build.sh
+++ build.sh
@@ -2,15 +2,17 @@
 
 currentWorkingDir=$(pwd)
 rm ./chat/*.pb.go
+rm ./msg/*.pb.go
+rm ./msg/*_helpers.go
+
 
 # Create 'msg' package
-cd ./chat/proto/ || exit
-protoc  -I="${currentWorkingDir}"/vendor -I=.  -I="${GOPATH}"/src --gogofaster_out=plugins=grpc:../ ./*.proto
-protoc  -I="${currentWorkingDir}"/vendor -I=.  -I="${GOPATH}"/src --gohelpers_out=../ ./*.proto
-protoc  -I="${currentWorkingDir}"/vendor -I=.  -I="${GOPATH}"/src --gokk_out=../ ./*.proto
-cd ..
+cd ./proto/ || exit
+protoc  -I="${currentWorkingDir}"/vendor -I=.  -I="${GOPATH}"/src --gogofaster_out=plugins=grpc:../msg ./*.proto
+protoc  -I="${currentWorkingDir}"/vendor -I=.  -I="${GOPATH}"/src --gohelpers_out=../msg ./*.proto
+protoc  -I="${currentWorkingDir}"/vendor -I=.  -I="${GOPATH}"/src --gokk_out=../msg ./*.proto
+cd ../msg
 go fmt
-
 
 # Create Notification gRPC Stubs
 cd "${currentWorkingDir}"/notif/proto/ || exit

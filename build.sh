fed6a66964e2145c8581052ecc3724a1f716f291
Ehsan N. Moosa <ehsan.code@nested.me>
2020-12-09 13:36:52 +0330

    rebuilt

--- build.sh
+++ build.sh
@@ -13,24 +13,4 @@ protoc  -I="${currentWorkingDir}"/vendor -I=.  -I="${GOPATH}"/src --gohelpers_ou
 protoc  -I="${currentWorkingDir}"/vendor -I=.  -I="${GOPATH}"/src --gokk_out=../go/msg ./*.proto
 cd "$currentWorkingDir"/go/msg || exit
 go fmt
-cd "$currentWorkingDir"
-
-
-## Create 'msg' package compatible with 'Rony'
-## For Rony we must ignore lines
-# 1. option (gogoproto.goproto_enum_prefix_all) = false;
-# 2. import "github.com/gogo/protobuf/gogoproto/gogo.proto";
-cd ./proto || exit
-regex='^(option \(gogoproto\.goproto_enum_prefix_all\) = false;|import "github\.com/gogo/protobuf/gogoproto/gogo\.proto";)$'
-mkdir tmp
-for proto in ./*.proto; do
-  grep -Ev "$regex" "$proto" >> ./tmp/"$proto"
-done
-cd ./tmp || exit
-protoc  -I=.  --go_out=../../go/rony/msg ./*.proto
-protoc  -I=.  --gorony_out=../../go/rony/msg ./*.proto
-protoc  -I=.  --gokk_out=../../go/rony/msg ./*.proto
-cd "$currentWorkingDir"/go/rony/msg || exit
-go fmt
 cd "$currentWorkingDir" || exit
-rm -r "$currentWorkingDir"/proto/tmp

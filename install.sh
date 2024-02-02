7edcc706a94c5e74a77b3186b474ac57dc2d7a31
Ehsan Noureddin Moosa <ehsannm@users.noreply.github.com>
2024-02-02 12:42:10 +0330

    rebuild proto

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@
 ### Make sure you have protoc installed on your system.
 ##### 2
 
-go install ./tools/protoc-gen-gohelpers
+#go install ./tools/protoc-gen-gohelpers
 go install ./tools/protoc-gen-gokk
